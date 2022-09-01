32ea677bd35cfa2937d0f8eb092e5b8ff0e5850e
F-Rivers <fxjxrx@gmail.com>
2022-09-01 12:58:42 -0700

    working v-12

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -1,5 +1,5 @@
 import { useContext, useState } from "react";
-import CartContext from "../context/shopContext";
+import { CartContext } from "../context/shopContext";
 import { formatter } from "../utils/helpers";
 import ProductOptions from "./ProductOptions";
 
